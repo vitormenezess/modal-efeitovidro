18122ba779c6c6bbaf87a58b4423cb7f71ac1207
vitormenezess <vitormenezes43@icloud.com>
2022-05-07 15:16:54 -0300

    07/05

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const login = document.querySelector(".login");
 const modalClose = document.querySelector(".close");
 const modalContainer = document.querySelector(".modal-container");
+const enviar = document.querySelector(".enviar");
 
 login.addEventListener("click", () => {
   modalContainer.classList.add("show");
@@ -9,3 +10,16 @@ login.addEventListener("click", () => {
 modalClose.addEventListener("click", () => {
   modalContainer.classList.remove("show");
 });
+const saveUser = () => {
+  const user = {
+    nome: document.getElementById("name").value,
+    cpf: document.getElementById("cpf").value,
+    email: document.getElementById("email").value,
+    senha: document.getElementById("password").value,
+    confirmPassword: document.getElementById("confirm-password").value,
+  };
+ 
+};
+
+enviar.addEventListener("click", saveUser());
+
