98d301ca79b02f532d8374fc7ad4af6063f0c18b
vitormenezess <vitormenezes43@icloud.com>
2022-05-09 15:45:56 -0300

    09/05

--- script.js
+++ script.js
@@ -10,16 +10,80 @@ login.addEventListener("click", () => {
 modalClose.addEventListener("click", () => {
   modalContainer.classList.remove("show");
 });
-const saveUser = () => {
-  const user = {
-    nome: document.getElementById("name").value,
-    cpf: document.getElementById("cpf").value,
-    email: document.getElementById("email").value,
-    senha: document.getElementById("password").value,
-    confirmPassword: document.getElementById("confirm-password").value,
-  };
- 
-};
 
-enviar.addEventListener("click", saveUser());
+//Formulario
+const form = document.getElementById("form");
+const nome = document.getElementById("name");
+const cpf = document.getElementById("cpf");
+const email = document.getElementById("email");
+const senha = document.getElementById("password");
+const confirmeSenha = document.getElementById("confirm-password");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const nomeValue = nome.value;
+  const cpfValue = cpf.value;
+  const emailValue = email.value;
+  const senhaValue = senha.value;
+  const confirmeSenhaVlaue = confirmeSenha.value;
+  //nome
+  if (nomeValue === "") {
+    setError(nome);
+  } else {
+    setSueccess(nome);
+  }
+  //cpf
+  if (cpfValue === "") {
+    setError(cpf);
+  } else {
+    setSueccess(cpf);
+  }
+  //email
+  if (emailValue === "") {
+    setError(email);
+  } else if (!emailIsValid(emailValue)) {
+    setError(email);
+  } else {
+    setSueccess(email);
+  }
+  //senha
+  if (senhaValue === "") {
+    setError(senha);
+  } else if (senhaValue.length < 4) {
+    setError(senha);
+  } else {
+    setSueccess(senha);
+  }
+  //confirmeSenha
+  if (confirmeSenhaVlaue === "") {
+    setError(confirmeSenha);
+  } else if (confirmeSenhaVlaue != senhaValue) {
+    setError(confirmeSenha);
+  } else {
+    setSueccess(confirmeSenha);
+  }
+  const formControls = document.querySelectorAll(".form-control");
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+  if (formIsValid) {
+    alert("Cadastro feito");
+  }
+}
+
+function setError(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+}
+function setSueccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+function emailIsValid(email) {
+  return /\S+@\S+\.\S+/.test(email);
+}
